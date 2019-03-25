d747a2d8ad656a9ae56fe35589abc7efe10bca52
Andranique Boone <andranique.boone@gmail.com>
2019-03-25 02:04:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = 0
-  min_key = nil
   name_hash.collect do |key, value|
+    min_value = 0
+    min_key = nil
     return key
   end
 end
